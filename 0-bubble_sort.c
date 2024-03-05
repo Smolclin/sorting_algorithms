ed1d1f4b50362a4ed29a49f1e8eead3f7638b9cc
Smolclin <clinsmol10@gmail.com>
2024-03-05 09:40:11 -0800

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - swaps two integers in an array
+ * @a: the first integer
+ * @b: the second integer
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - sorts the array of integers in ascending oreder
+ * @array: array of integers to sort
+ * @size: size of the array
+ *
+ * Description: print the array after each swap
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t  i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i,array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
---
+++ sort.h
@@ -0,0 +1,42 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define up 0
+#define down 1
+
+/**
+ * enum bool - enumeration of boolen values
+ * @false: equals 0
+ * @true: equals 1
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+}bool;
+
+/**
+ * struct listint_s - doubly linked list node
+ * @n: node integer
+ * @prev: pointer to previous elements in the list
+ * @next: pointer to next element in the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+}listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
